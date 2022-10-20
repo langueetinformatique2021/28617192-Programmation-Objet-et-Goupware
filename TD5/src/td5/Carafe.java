a90f3dd4272ae5236a626e447d69fc663c0776df
Marceau-h <marceau8@gmail.com>
2022-10-20 10:40:26 +0200

    TD5 changements + nettoyage

--- TD5/src/td5/Carafe.java
+++ TD5/src/td5/Carafe.java
@@ -33,6 +33,14 @@ public class Carafe {
 		}
 	}
 	
+	public void Vider() {
+		contenu_ = 0;
+	}
+	
+	public void Remplir() {
+		contenu_ = capacite_;
+	}
+	
 	public int Contenu() {
 		return contenu_;
 	}
@@ -40,5 +48,24 @@ public class Carafe {
 	public int Capacite() {
 		return capacite_;
 	}
+	
+	public void Transvaser(Carafe c1, Carafe c2) {
+		int cont1 = c1.Contenu();
+		int cont2 = c2.Contenu();
+		int cap2 = c2.Capacite();
+		
+		int maxTrans = cap2 - cont2;
+		if (maxTrans > cont1) {
+			c2.Remplir(cont1);
+			c1.Vider();
+		}
+		else {
+			c2.Remplir(maxTrans);
+			c1.Vider(maxTrans);
+		}
+		
+	}
+	
+	
 
 }
--- TD5/src/td5/TestCarafe.java
+++ TD5/src/td5/TestCarafe.java
@@ -1,5 +1,7 @@
 package td5;
 
 public class TestCarafe {
-
+	Carafe c1 = new Carafe(1, 1);
+	Carafe c2 = new Carafe(1, 2);
+	
 }
