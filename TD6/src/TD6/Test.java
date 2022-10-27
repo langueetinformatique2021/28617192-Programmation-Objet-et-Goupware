efe2551980bae322a8c9f65655aa26dd1436f3e4
Marceau-h <marceau8@gmail.com>
2022-10-27 10:40:31 +0200

    Tests encore

--- TD6/src/TD6/Test.java
+++ TD6/src/TD6/Test.java
@@ -4,10 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		Chat c = new Chat("Persan");
-		System.out.println(c.domestique);
-		c.domestiquer("Chien"); /** Un chat qui s'appelle chien.. Pourquoi pas? */
-		System.out.println(c.domestique);
+		System.out.print(c.domestique + " ");
 		System.out.println(c.nom());
+		c.domestiquer("Chien"); /** Un chat qui s'appelle chien.. Pourquoi pas? */
+		System.out.print(c.domestique + " ");
+		System.out.println(c.nom());
+		c.présente();
+		c.crie();
 
 }
 	
