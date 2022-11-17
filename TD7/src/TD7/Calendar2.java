ac54cc233559cc6a8f4f7b7f55b2c02c802b958c
Marceau-h <marceau8@gmail.com>
2022-11-17 10:45:42 +0100

    Calendar2

---
+++ TD7/src/TD7/Calendar2.java
@@ -0,0 +1,56 @@
+package TD7;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Calendar2 extends Calendar {
+	protected Date creation;
+	boolean cours;
+	
+	public Calendar2() {
+		super();
+		creation = new Date();
+		cours = _iscours();
+	}
+	
+	private boolean _iscours() {
+		if (9 < this.creation.getHours() && this.creation.getHours() < 13) {
+			return true;
+		} else {
+			if (9 == this.creation.getHours() && 29 < this.creation.getMinutes()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean iscours() {
+		return cours;
+	}
+	
+	private String TimeToString(Date d) {
+		String H = Integer.toString(d.getHours());
+		String M = Integer.toString(d.getMinutes());
+		String S = Integer.toString(d.getSeconds());
+		return H + " heures " + M + " minutes " + S + " secondes";						
+	}
+	
+	private String DateToString(Date d) {
+		String D = Integer.toString(d.getDay());
+		String M = Integer.toString(d.getMonth());
+		String Y = Integer.toString(d.getYear()+1900);
+		return D + "/" + M + "/" + Y;	
+
+	}
+	
+	public void creation() {
+		System.out.println(TimeToString(this.creation));
+	}
+	
+	public void afficher() {
+		System.out.println(DateToString(new Date()));
+		System.out.println(TimeToString(new Date()));
+	}
+
+
+}
--- TD7/src/TD7/Main.java
+++ TD7/src/TD7/Main.java
@@ -9,16 +9,22 @@ public class Main {
 		Date a = new Date();
 		GregorianCalendar b = new GregorianCalendar();
 
-		System.out.println(a);
-		System.out.println(b.get(Calendar.HOUR));
-		System.out.println(b.get(Calendar.MINUTE));
-		System.out.println(b.get(Calendar.SECOND));
-
-		System.out.println(b.get(Calendar.DATE));
-		System.out.println(b.get(Calendar.MONTH));
-		System.out.println(b.get(Calendar.YEAR));
+//		System.out.println(a);
+//		System.out.println(b.get(Calendar.HOUR));
+//		System.out.println(b.get(Calendar.MINUTE));
+//		System.out.println(b.get(Calendar.SECOND));
+//
+//		System.out.println(b.get(Calendar.DATE));
+//		System.out.println(b.get(Calendar.MONTH));
+//		System.out.println(b.get(Calendar.YEAR));
+//		
+//		System.out.println(b.getTime());
 		
-		System.out.println(b.getTime());
+		Calendar2 c = new Calendar2();
+		c.creation();
+		c.afficher();
+		System.out.println(c.iscours());
+		
 
 		
 	}
