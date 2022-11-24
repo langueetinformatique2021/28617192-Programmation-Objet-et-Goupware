0ce5dbab61f7a158deb2b40fbe62ccf9837ccbd6
Marceau-h <marceau8@gmail.com>
2022-11-24 10:12:39 +0100

    PileFace

--- TD8/src/TD8/Aleat.java
+++ TD8/src/TD8/Aleat.java
@@ -4,10 +4,11 @@ package TD8;
  * @author montacie
  */
 public class Aleat {
-	
+	/**Le maximum défini lors de l'instanciation*/
 	private int maximum;
 	
-	/** instanciation 
+	/** 
+	 * Constructeur de la classe, prend un entier en entrée
 	 * @param maximum nombre maximum généré 
 	 */
 	public Aleat(int maximum) {
@@ -16,7 +17,7 @@ public class Aleat {
 
 	/** 
 	 * Méthode retournant un entier aléatiore à partir du maximum de l'instance de la classe
-	 * @return nombre aleatoire 
+	 * @return nombre aleatoire compris entre 0 et le maximum
 	 */
 	public int get() {
 		return (int)(Math.random() * this.maximum) + 1;
