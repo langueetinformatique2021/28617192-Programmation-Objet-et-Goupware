e63dea99cfe687fa11d4f86b595e2236417da110
Marceau-h <marceau8@gmail.com>
2022-11-24 09:51:28 +0100

    TD8 - EX1 - QU3

---
+++ TD8/src/TD8/Division2.java
@@ -0,0 +1,16 @@
+package TD8;
+
+public class Division2 {
+	public static void main(String[] args) { division(); }
+	
+	public static void division() throws ArithmeticException {
+		try {
+		int i = 1000, j;
+		do {i--; j = 1 / i;} while (true);
+		} catch (ArithmeticException e){
+			System.out.println("Division par 0 !");
+			throw e;
+		}
+	}
+
+}
