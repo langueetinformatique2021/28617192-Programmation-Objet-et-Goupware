7999fcb4e684739f2e438792a19498bdd1730045
Marceau-h <marceau8@gmail.com>
2022-11-24 10:57:01 +0100

    Classe LancerDes + TestDes La classe LAncerDes hérite de Aleat pour implémenter 2 méthodes, celle pour générer 3 lancers et une seconde pour provoquer l'exception E421

--- TD8/src/TD8/LancerDes.java
+++ TD8/src/TD8/LancerDes.java
@@ -2,22 +2,34 @@ package TD8;
 
 import java.util.ArrayList;
 
-public class LancerDes {
-
-	public static void main(String[] args) throws E421 {
-		String nom = "TEST";
-		Aleat a = new Aleat(6);
-		
-				
-		do {
-			ArrayList<Integer> al = a.lancerDes(nom);
-			print(al);
-		} while (true);
-		
-
+public class LancerDes extends Aleat {
+	
+	String nom = "TEST";
+	int faces = 6;
+	
+	public LancerDes(String nom, int faces) {
+		super(faces);
+		this.faces = faces;
+		this.nom = nom;
 	}
 	
-	public static void print(Object o) {System.out.println(o);}
+
+	public ArrayList<Integer> lancerDes() throws E421 {
+		ArrayList<Integer> list = new ArrayList<>();
+		int i = 0;
+		do {
+			i++;
+			list.add(this.get());
+		} while (i < 3);
+		this.test421(list, nom);
+		return list;
+				
+	}
+	
+	private void test421(ArrayList<Integer> al, String nom) throws E421 {
+		if (al.contains(4)) if (al.contains(2)) if (al.contains(1)) throw new E421(nom, al);
+		}
+
 
 
 }
