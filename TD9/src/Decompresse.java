6154058ee2d471ffc981821f0cb9b649633ebf86
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2022-12-01 10:49:01 +0100

    TD9 EX2 'decompresse'

--- TD9/src/Decompresse.java
+++ TD9/src/Decompresse.java
@@ -7,14 +7,16 @@ public class Decompresse {
         File file = new File("Nom2.zip");
         File file2 = new File("Nomdezip.txt");
         try {
-            InputStream in = new BufferedInputStream(new FileInputStream(file));
-            OutputStream out = new BufferedOutputStream(new InflaterOutputStream(new FileOutputStream(file2)));
-            byte[] buffer = new byte[1024];
-            int lengthRead;
-            while ((lengthRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, lengthRead);
-                out.flush();
+            InputStream in = new FileInputStream(file);
+            OutputStream out = new FileOutputStream(file2);
+            InflaterOutputStream infOut = new InflaterOutputStream(out);
+            int content;
+            while ((content = in.read()) != -1) {
+                infOut.write(content);
             }
+            infOut.close();
+            in.close();
+            out.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
