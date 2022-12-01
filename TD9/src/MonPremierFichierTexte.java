bd55567908370905853765a33048d7fe3fa5cf50
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2022-12-01 10:14:01 +0100

    TD9 EX1 suite

--- TD9/src/MonPremierFichierTexte.java
+++ TD9/src/MonPremierFichierTexte.java
@@ -1,6 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.*;
 import java.util.ArrayList;
 
 public class MonPremierFichierTexte {
@@ -14,14 +12,40 @@ public class MonPremierFichierTexte {
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("Nom.txt"));
         writinglist(al, writer);
-
         writer.close();
+
+        BufferedReader reader = new BufferedReader(new FileReader("Nom.txt"));
+        boolean x = checkList(al, reader);
+        reader.close();
+        System.out.println(x);
+
+
     }
     public static void writinglist(ArrayList<String> al, BufferedWriter writer) throws IOException {
         for (String str : al) {
             writer.write(str);
+            writer.newLine();
             }
      }
+
+     public static boolean checkList(ArrayList<String> al, BufferedReader reader) throws IOException {
+        // for each line in the file, check if it is in the list
+
+         for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            if (al.contains(line)) {
+                continue;
+            }
+            else {
+                return false;
+            }
+
+         }
+         return true;
+
+
+         }
+
+
 }
 
 
