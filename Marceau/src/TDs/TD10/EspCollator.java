fd71cbf239c42d67b7ae86c102e09567cea9a307
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2022-12-15 11:02:31 +0100

    TD10 ex 3 suite

--- Marceau/src/TDs/TD10/EspCollator.java
+++ Marceau/src/TDs/TD10/EspCollator.java
@@ -1,11 +1,15 @@
 package TDs.TD10;
 
+import java.text.ParseException;
+import java.text.RuleBasedCollator;
 import java.util.List;
 
-public class EspCollator {
-    private final List<String> Collator = List.of("A B C Ĉ D E F G Ĝ H Ĥ I J Ĵ K L M N O P R S Ŝ T U Ŭ V Z".split(" "));
+public class EspCollator extends RuleBasedCollator {
+    private final String collator = "A B C Ĉ D E F G Ĝ H Ĥ I J Ĵ K L M N O P R S Ŝ T U Ŭ V Z";
+    private final List<String> Collator = List.of(this.collator.split(" "));
 
-    public EspCollator() {
+    public EspCollator() throws ParseException {
+        super("A B C Ĉ D E F G Ĝ H Ĥ I J Ĵ K L M N O P R S Ŝ T U Ŭ V Z");
     }
 
     public boolean compatible() {
--- Marceau/src/TDs/TD10/TestCollator.java
+++ Marceau/src/TDs/TD10/TestCollator.java
@@ -1,9 +1,15 @@
 package TDs.TD10;
 
 import java.text.Collator;
+import java.text.ParseException;
+import java.text.RuleBasedCollator;
 
 public class TestCollator {
-    EspCollator EC = new EspCollator();
-    // C = new Collator();
+    public static void main(String[] args) throws ParseException {
+        EspCollator EC= new EspCollator();
+        Collator C = Collator.getInstance();
+        Collator RBC = RuleBasedCollator.getInstance();
+
+    }
 
 }
